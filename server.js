12f5e4d877b320b8f829aa43680129ba175b7218
Chris Underwood <cunderwoodmn@gmail.com>
2020-05-19 20:32:35 -0400

    create first server file and gitignore

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+//load env variables
+dotenv.config({ path: './config/config.env' });
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running on port ${PORT} in ${process.env.NODE_ENV} mode.`)
+);
