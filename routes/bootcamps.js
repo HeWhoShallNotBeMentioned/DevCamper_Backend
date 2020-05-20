a17109fa9721c949411189702e9bf12349cd9dc0
Chris Underwood <cunderwoodmn@gmail.com>
2020-05-19 21:46:31 -0400

    moved bootcamp routes to their own routes file

---
+++ routes/bootcamps.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+  res.status(200).json({ success: true, message: 'Show all bootcamps.' });
+});
+
+router.get('/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, message: `Display one bootcamp ${req.params.id}` });
+});
+
+router.post('/', (req, res) => {
+  res.status(200).json({ success: true, message: 'Create new bootcamp.' });
+});
+
+router.put('/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, message: `Update bootcamp ${req.params.id}` });
+});
+
+router.delete('/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, message: `Delete bootcamp ${req.params.id}` });
+});
+
+module.exports = router;
