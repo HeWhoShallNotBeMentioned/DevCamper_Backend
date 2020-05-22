f5f4f4690ca8b3339df46a0a0ed3c92002da1f11
Chris Underwood <cunderwoodmn@gmail.com>
2020-05-22 19:16:50 -0400

    complete route to retreive all bootcamps

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,8 +3,19 @@ const Bootcamp = require('../models/Bootcamp');
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, message: 'Show all bootcamps.' });
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcampsReply = await Bootcamp.find();
+    res
+      .status(200)
+      .json({
+        success: true,
+        message: 'Retreived all bootcamps',
+        data: bootcampsReply,
+      });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
 };
 
 // @desc    Get single bootcamp
