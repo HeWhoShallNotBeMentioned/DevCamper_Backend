0c85e225ef27b36c2739f77630ed9516d9f81f6c
Chris Underwood <cunderwoodmn@gmail.com>
2020-06-10 22:11:49 -0400

    add post route to create course.

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,6 +1,7 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const Course = require('../models/Course');
+const Bootcamp = require('../models/Bootcamp');
 
 // @desc    Get all courses
 // @route   GET /api/v1/courses
@@ -45,6 +46,29 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
   res.status(200).json({
     success: true,
     message: 'Retrieved course.',
+    data: course,
+  });
+});
+
+// @desc    Add a single course
+// @route   POST /api/v1/bootcamps/:bootcampId/courses
+// @access  Private
+exports.createCourse = asyncHandler(async (req, res, next) => {
+  req.body.bootcamp = req.params.bootcampId;
+
+  const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`No bootcamp with id:   ${req.params.bootcampId}.`, 404)
+    );
+  }
+
+  const course = await Course.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    message: 'Course Created.',
     data: course,
   });
 });
--- routes/courses.js
+++ routes/courses.js
@@ -1,8 +1,12 @@
 const router = require('express').Router({ mergeParams: true });
 
-const { getCourses, getCourse } = require('../controllers/courses');
+const {
+  getCourses,
+  getCourse,
+  createCourse,
+} = require('../controllers/courses');
 
-router.route('/').get(getCourses);
+router.route('/').get(getCourses).post(createCourse);
 router.route('/:id').get(getCourse);
 
 module.exports = router;
