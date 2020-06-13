20faf8b3e62a641f945ec61d881a5ac54670066f
Chris Underwood <cunderwoodmn@gmail.com>
2020-06-13 19:30:52 -0400

    finish average cost static methid including fixing numerous bugs.

--- models/Course.js
+++ models/Course.js
@@ -39,25 +39,36 @@ const CourseSchema = new mongoose.Schema({
 });
 
 // Static method to get avg of course tuitions
-CourseSchema.statics.getAverageCost -
-  async function (bootcampId) {
-    console.log('Calculating avg cost,,,'.blue);
+CourseSchema.statics.getAverageCost = async function (bootcampId) {
+  console.log('Calculating avg cost,,,'.blue);
 
-    const obj = await this.aggregate([
-      {
-        $match: { bootcamp: bootcampId },
-      },
-      {
-        $group: { _id: '$bootcampId', averageCost: { $avg: 'tuiton' } },
-      },
-    ]);
-    console.log('averageCost Obj -----', obg);
-  };
+  const arrObj = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId },
+    },
+    {
+      $group: { _id: '$bootcamp', averageCost: { $avg: '$tuition' } },
+    },
+  ]);
+  console.log('averageCost Obj -----', arrObj);
+
+  try {
+    await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+      averageCost: Math.ceil(arrObj[0].averageCost / 10) * 10,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 // Call getAverageCost after save
-CourseSchema.post('save', function () {});
+CourseSchema.post('save', function () {
+  this.constructor.getAverageCost(this.bootcamp);
+});
 
 // Call getAverageCost before remove
-CourseSchema.post('remove', function () {});
+CourseSchema.post('remove', function () {
+  this.constructor.getAverageCost(this.bootcamp);
+});
 
 module.exports = mongoose.model('Course', CourseSchema);
