2df6003c54391da743e974bc61063631ed24a765
Chris Underwood <cunderwoodmn@gmail.com>
2020-06-14 18:29:11 -0400

    created middleware from code in bootcamps.js that allowed users to select specific fields to return, results to be sorted, pagination, and number of items per page.

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,7 +3,6 @@ const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const geocoder = require('../utils/geocoder');
 const Bootcamp = require('../models/Bootcamp');
-const { findByIdAndUpdate } = require('../models/Bootcamp');
 
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
---
+++ middleware/advancedResults.js
@@ -0,0 +1,80 @@
+const advancedResults = (model, populate) => async (req, res, next) => {
+  let query;
+
+  // Copy req.query
+  const reqQuery = { ...req.query };
+
+  // Fields to exclude
+  const removeFields = ['select', 'sort', 'page', 'limit'];
+
+  // Loop over removeFields and delete them from reqQuery
+  removeFields.forEach(param => delete reqQuery[param]);
+
+  console.log(reqQuery);
+
+  // Create query string
+  let qp = JSON.stringify(reqQuery);
+
+  // Create operators ($gt, $gte, $lt, $lte, $in)
+  qp = qp.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+  // Convert string back to JSON
+  let queryStr = JSON.parse(qp);
+
+  query = model.find(queryStr);
+
+  // Select Fields
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+
+    query.select(fields);
+  }
+
+  // Sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query.sort(sortBy);
+  } else {
+    query.sort('-createdAt');
+  }
+
+  // Pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 20;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await model.countDocuments();
+
+  query.skip(startIndex).limit(limit);
+
+  if (populate) {
+    query.populate(populate);
+  }
+
+  // Database search based on our query params
+  const queryReply = await query;
+
+  // Pagination result
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = { page: page + 1, limit };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    };
+  }
+  res.advancedResults = {
+    success: true,
+    count: queryReply.length,
+    pagination,
+    data: queryReply,
+  };
+
+  next();
+};
+
+module.exports = advancedResults;
