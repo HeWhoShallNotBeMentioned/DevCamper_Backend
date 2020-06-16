99acd9e5c4852d82eca2dc8e7fd9ab29719476d5
Chris Underwood <cunderwoodmn@gmail.com>
2020-06-16 19:55:45 -0400

    completed route to be able to add a new user.

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,5 +6,17 @@ const User = require('../models/User');
 // @route   GET /api/v1/auth/register
 // @access  Public
 exports.register = asyncHandler(async (req, res, next) => {
-  res.status(200).json({ success: true, message: 'Registered new user.' });
+  const { name, email, password, role } = req.body;
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+  });
+
+  res
+    .status(200)
+    .json({ success: true, message: 'Registered new user.', user: user });
 });
