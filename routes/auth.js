1d4d22ef123a7cd142be0cd8607ba30ee485ff53
Chris Underwood <cunderwoodmn@gmail.com>
2020-06-17 21:57:53 -0400

    add login route and controller with a helper method on the User model.

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
-const { register } = require('../controllers/auth');
+const { register, login } = require('../controllers/auth');
 
 router.route('/register').post(register);
+
+router.route('/login').post(login);
 
 module.exports = router;
