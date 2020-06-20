30821c4988184974ba3e360d1dbd3f209927abff
Chris Underwood <cunderwoodmn@gmail.com>
2020-06-19 21:49:02 -0400

    created middleware to protect routers for authenticated user only.

---
+++ middleware/auth.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtiken');
+const asyncHandler = require('./async');
+const ErrorRespnse = require('../utils/errorResponse');
+const User = require('../models/User');
+const ErrorResponse = require('../utils/errorResponse');
+
+// Protect Routes
+exports.protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  //else if (req.cookies.token) {
+  //   token = req.cookies.token;
+  // }
+
+  if (!token) {
+    return next(new ErrorResponse('Not authorized to access this route.', 401));
+  }
+
+  try {
+    // Verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log('decoded jwt-----', decoded);
+    req.user = await User.findById(decoded.id);
+    next();
+  } catch (error) {
+    return next(new ErrorResponse('Not authorized to access this route.', 401));
+  }
+});
