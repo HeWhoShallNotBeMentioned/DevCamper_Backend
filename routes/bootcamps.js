8296cbf28cd054c7eae936ac7274c3fef1d8dd00
Chris Underwood <cunderwoodmn@gmail.com>
2020-06-20 23:33:46 -0400

    implented authorize middleware on bootcamps and courses.

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -16,7 +16,7 @@ const {
   bootcampPhotoUpload,
 } = require('../controllers/bootcamps');
 
-const { protect } = require('../middleware/auth');
+const { protect, authorize } = require('../middleware/auth');
 
 const Bootcamp = require('../models/Bootcamp');
 
@@ -25,16 +25,18 @@ const advancedResults = require('../middleware/advancedResults');
 router
   .route('/')
   .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
-  .post(protect, createBootcamp);
+  .post(protect, authorize('publisher', 'admin'), createBootcamp);
 
 router
   .route('/:id')
   .get(getBootcamp)
-  .put(protect, updateBootcamp)
-  .delete(protect, deleteBootcamp);
+  .put(protect, authorize('publisher', 'admin'), updateBootcamp)
+  .delete(protect, authorize('publisher', 'admin'), deleteBootcamp);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
-router.route(`/:id/photo`).put(protect, bootcampPhotoUpload);
+router
+  .route(`/:id/photo`)
+  .put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload);
 
 module.exports = router;
--- routes/courses.js
+++ routes/courses.js
@@ -8,7 +8,7 @@ const {
   deleteCourse,
 } = require('../controllers/courses');
 
-const { protect } = require('../middleware/auth');
+const { protect, authorize } = require('../middleware/auth');
 const Course = require('../models/Course');
 const advancedResults = require('../middleware/advancedResults');
 
@@ -21,12 +21,12 @@ router
     }),
     getCourses
   )
-  .post(protect, createCourse);
+  .post(protect, authorize('publisher', 'admin'), createCourse);
 
 router
   .route('/:id')
   .get(getCourse)
-  .put(protect, updateCourse)
-  .delete(protect, deleteCourse);
+  .put(protect, authorize('publisher', 'admin'), updateCourse)
+  .delete(protect, authorize('publisher', 'admin'), deleteCourse);
 
 module.exports = router;
