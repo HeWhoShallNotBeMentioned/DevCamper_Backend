acb3299a22aac1645193091212ad01b4e2ad3154
Chris Underwood <cunderwoodmn@gmail.com>
2020-06-30 20:40:39 -0400

    finished setting up ythe route to send email to reset password.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
+const sendEmail = require('../utils/sendEmail');
 const User = require('../models/User');
 
 // @desc    Register user
@@ -75,7 +76,27 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
 
   const notUsedResponse = await user.save({ validateBeforeSave: false });
 
-  res.status(200).json({ success: true, data: user });
+  // Create reset url
+  const resetUrl = `${req.protocol}://${req.get(
+    'host'
+  )}/api/vi/resetpassword/${resetToken}`;
+
+  const message = `You are receiving this email because you (or someone else) has requested the reset of a password. Please make a PUT requrest to: \n\n ${resetUrl}`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Password reset token',
+      message,
+    });
+    res.status(200).json({ success: true, data: 'Email sent.' });
+  } catch (error) {
+    user.getResetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+    const notUsedResponseUser = await user.save({ validateBeforeSave: false });
+
+    return next(new ErrorResponse(`Email could not be sent.`, 500));
+  }
 });
 
 // Get token from model, create cookie, and send response
