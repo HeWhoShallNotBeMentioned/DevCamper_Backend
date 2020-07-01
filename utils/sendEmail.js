7d3485e3cfbd434b412ecca46074f5cb4c417879
Chris Underwood <cunderwoodmn@gmail.com>
2020-06-30 20:10:50 -0400

    add nodemailer npm package and crate sendEmail.js utility file

---
+++ utils/sendEmail.js
@@ -0,0 +1,28 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async options => {
+  // create reusable transporter object using the default SMTP transport
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.SMTP_EMAIL,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
+
+  const message = {
+    from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+
+  const info = await transporter.sendMail(message);
+
+  console.log('Message sent: %s', info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+};
+
+module.exports = sendEmail;
