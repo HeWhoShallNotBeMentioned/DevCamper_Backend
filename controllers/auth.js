4552c27309ea578d15997b0a4e4725637256ca86
Chris Underwood <cunderwoodmn@gmail.com>
2020-07-01 21:00:46 -0400

    create updateDetails controler

--- controllers/auth.js
+++ controllers/auth.js
@@ -51,8 +51,24 @@ exports.login = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
+// @desc    Update user details
+// @route   PUT /api/v1/auth/
+// @access  Private
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+  const fieldsToUpdate = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+  const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({ success: true, data: user });
+});
+
 // @desc    Get current user
-// @route   GET /api/v1/auth/me
+// @route   GET /api/v1/auth/updatedetails
 // @access  Private
 exports.getMe = asyncHandler(async (req, res, next) => {
   const user = await User.findById(req.user.id);
