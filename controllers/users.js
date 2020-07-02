b03e05e8e3b25b0aefeec75f3fd3fd88d567d759
Chris Underwood <cunderwoodmn@gmail.com>
2020-07-02 19:36:14 -0400

    create users controller and get all users route.

---
+++ controllers/users.js
@@ -0,0 +1,12 @@
+const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
+const User = require('../models/User');
+
+// @desc    Get all users
+// @route   GET /api/v1/users/
+// @access  Private/Admin
+exports.getUsers = asyncHandler(async (req, res, next) => {
+  res
+    .status(200)
+    .json({ ...res.advancedResults, message: 'Retrieved all users.' });
+});
