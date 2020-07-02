3da1e4627ebd64593a109a610631bd3196c0ef3d
Chris Underwood <cunderwoodmn@gmail.com>
2020-07-01 21:21:27 -0400

    add route for updatedetails

--- routes/auth.js
+++ routes/auth.js
@@ -5,18 +5,16 @@ const {
   getMe,
   forgotPassword,
   resetPassword,
+  updateDetails,
 } = require('../controllers/auth');
 
 const { protect } = require('../middleware/auth');
 
 router.route('/register').post(register);
-
 router.route('/login').post(login);
-
 router.route('/me').get(protect, getMe);
-
+router.route('/updatedetails').put(protect, updateDetails);
 router.route('/forgotpassword').post(forgotPassword);
-
 router.route('/resetpassword/:resettoken').put(resetPassword);
 
 module.exports = router;
