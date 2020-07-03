0a36cf298a33315e6747112fd59ec1a11f317e2a
Chris Underwood <cunderwoodmn@gmail.com>
2020-07-02 22:43:51 -0400

    create user routes file and create basic routes

---
+++ routes/users.js
@@ -0,0 +1,23 @@
+const router = require('express').Router({ mergeParams: true });
+
+const {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require('../controllers/uers');
+
+const { protect, authorize } = require('../middleware/auth');
+
+const User = require('../models/User');
+const advancedResults = require('../middleware/advancedResults');
+
+router.use(protect);
+router.use(authorize('admin'));
+
+router.route('./').get(advancedResults(User), getUsers).post(createUser);
+
+router.route('/:id').get(getUser).put(updateUser).delete(deleteUser);
+
+module.exports = router;
