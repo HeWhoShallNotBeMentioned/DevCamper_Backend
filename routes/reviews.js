125d2b5d90d8c41a87b769f10ba5d6da087735f8
Chris Underwood <cunderwoodmn@gmail.com>
2020-07-05 20:40:08 -0400

    add create review route

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,18 +1,25 @@
 const router = require('express').Router({ mergeParams: true });
 
-const { getReviews, getReview } = require('../controllers/reviews');
+const {
+  getReviews,
+  getReview,
+  createReview,
+} = require('../controllers/reviews');
 
 const { protect, authorize } = require('../middleware/auth');
 const Review = require('../models/Review');
 const advancedResults = require('../middleware/advancedResults');
 
-router.route('/').get(
-  advancedResults(Review, {
-    path: 'bootcamp',
-    select: 'name description',
-  }),
-  getReviews
-);
+router
+  .route('/')
+  .get(
+    advancedResults(Review, {
+      path: 'bootcamp',
+      select: 'name description',
+    }),
+    getReviews
+  )
+  .post(proctect, authorize('user', 'admin'), createReview);
 
 router.route('/:id').get(getReview);
 
