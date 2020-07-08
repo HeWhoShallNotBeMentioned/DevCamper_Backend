bd6fba27e42c5f60044e031ebbc226bb97e09057
Chris Underwood <cunderwoodmn@gmail.com>
2020-07-07 21:34:49 -0400

    add logout route to auth

--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,7 @@ const {
   resetPassword,
   updateDetails,
   updatePassword,
+  logout,
 } = require('../controllers/auth');
 
 const { protect } = require('../middleware/auth');
@@ -14,6 +15,7 @@ const { protect } = require('../middleware/auth');
 router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/me').get(protect, getMe);
+router.route('/logout').get(protect, logout);
 router.route('/updatedetails').put(protect, updateDetails);
 router.route('/updatepassword').put(protect, updatePassword);
 router.route('/forgotpassword').post(forgotPassword);
