d3a42d5cffa5ef80d2cdfcd39262b6fb7c6d6e5f
Chris Underwood <cunderwoodmn@gmail.com>
2020-07-08 14:56:38 -0400

    add cors functionality

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const helmet = require('helmet');
 const xss = require('xss-clean');
 const rateLimit = require('express-rate-limit');
 const hpp = require('hpp');
+const cors = require('cors');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -63,6 +64,9 @@ app.use(limiter);
 
 // Prevent http param pollution
 app.use(hpp());
+
+// Enable CORS
+app.use(cors());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
