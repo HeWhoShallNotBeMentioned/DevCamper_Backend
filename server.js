bbffbbaefeb09e43e374d377092d6e447354f315
Chris Underwood <cunderwoodmn@gmail.com>
2020-07-08 14:04:08 -0400

    installed and implemented xss-clean to prevent cross site scripting

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const fileUpload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
+const xss = require('xss-clean');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -47,6 +48,9 @@ app.use(mongoSanitize());
 
 // Set Security Headers
 app.use(helmet());
+
+// Prevent Cross Site Scripting
+app.use(xss());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
