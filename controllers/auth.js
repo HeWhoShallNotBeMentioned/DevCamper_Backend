baef9011a3afced5ce34b1914fad48fa876df49c
Chris Underwood <cunderwoodmn@gmail.com>
2020-07-07 20:48:48 -0400

    changed middleware/auth to reflect the use of cookies instead of bearer/tokens. add logout to controllers

--- controllers/auth.js
+++ controllers/auth.js
@@ -85,12 +85,24 @@ exports.updatePassword = asyncHandler(async (req, res, next) => {
 });
 
 // @desc    Get current user
-// @route   GET /api/v1/auth/updatedetails
+// @route   GET /api/v1/auth/me
 // @access  Private
 exports.getMe = asyncHandler(async (req, res, next) => {
   const user = await User.findById(req.user.id);
 
   res.status(200).json({ success: true, data: user });
+});
+
+// @desc    Log user out / clear cookie
+// @route   GET /api/v1/auth/logout
+// @access  Private
+exports.logout = asyncHandler(async (req, res, next) => {
+  res.cookie('token', 'none', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+
+  res.status(200).json({ success: true, data: {} });
 });
 
 // @desc    Forgot password
